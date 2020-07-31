4bce066b1a3596547999e46a2b33c8e98d047816
misha gupta <guptamisha88@gmail.com>
2020-08-01 00:37:38 +0530

    First Commit

---
+++ SarsoShoppingData/PartialClasses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SarsoShoppingData
+{
+    public partial class MemberProfile
+    {
+        public int SponserID { get; set; }
+        public string CompanyName { get; set; }
+        public string MobileNo { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string cnfPassword { get; set; }
+    }
+}
---
+++ SarsoShoppingMVC/Scripts/Register.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    $("#SponserID").click(function () {
+        var id = $("#SponserID").val();
+        $(".preloader").show();
+        $.ajax({
+            url: '/Account/VerifySponsor',
+            type: 'GET',
+            data: { "SponsorId": id },
+            success: function (objResponse) {
+                if (objResponse != null && objResponse.result == "") {
+                    $("#SponserName").html(objResponse.name);
+                }
+                else {
+                    $("#SponsorErrorTxt").html(objResponse.name);
+                }
+            },
+            error: function (xhr, data) {
+
+                console.log(xhr);
+                console.log("Error:", data);
+            }
+        });
+    });
+
+    $("form[name=RegistrationForm]").bind('submit', function (e) {
+        $("#RegisterSubmit").attr("disabled", true);
+
+        var formdata = new FormData($("#RegistrationForm")[0]);
+        $.ajax({
+            url: '/Account/PartialInsertReg',
+            type: 'POST',
+            data: formdata,
+            success: function (objResponse) {
+                $("#RegisterSubmit").attr("disabled", false);
+            },
+            error: function (xhr, data) {
+
+                console.log(xhr);
+                console.log("Error:", data);
+            }
+        });
+    });
+});
