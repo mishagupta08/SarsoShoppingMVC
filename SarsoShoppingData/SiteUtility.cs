e2443d4c1616c3e300111e43d88f2496e94952d9
misha gupta <guptamisha88@gmail.com>
2020-08-05 14:55:41 +0530

    new files

---
+++ SarsoShoppingData/SiteUtility.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SarsoShoppingData
+{
+    public class SiteUtility
+    {
+        public string Encrypt(string toEncrypt)
+        {
+            byte[] keyArray;
+            byte[] toEncryptArray = Encoding.UTF8.GetBytes(toEncrypt);
+            var settingsReader = new AppSettingsReader();
+            var key = (string)settingsReader.GetValue("SarsoBiz", typeof(String));
+            {
+                var hashmd5 = new MD5CryptoServiceProvider();
+                keyArray = hashmd5.ComputeHash(Encoding.UTF8.GetBytes(key));
+                hashmd5.Clear();
+            }
+            var tdes = new TripleDESCryptoServiceProvider
+            {
+                Key = keyArray,
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            var cTransform = tdes.CreateEncryptor();
+            byte[] resultArray =
+              cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+            tdes.Clear();
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        public string Decrypt(string cipherString)
+        {
+            try
+            {
+                cipherString = cipherString.Replace(' ', '+');
+                byte[] keyArray;
+                byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+                var settingsReader = new AppSettingsReader();
+                var key = (string)settingsReader.GetValue("SarsoBiz", typeof(String));
+                {
+                    var hashmd5 = new MD5CryptoServiceProvider();
+                    keyArray = hashmd5.ComputeHash(Encoding.UTF8.GetBytes(key));
+                    hashmd5.Clear();
+                }
+                var tdes = new TripleDESCryptoServiceProvider
+                {
+                    Key = keyArray,
+                    Mode = CipherMode.ECB,
+                    Padding = PaddingMode.PKCS7
+                };
+                var cTransform = tdes.CreateDecryptor();
+                byte[] resultArray = cTransform.TransformFinalBlock(
+                    toEncryptArray, 0, toEncryptArray.Length);
+                tdes.Clear();
+                return Encoding.UTF8.GetString(resultArray);
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
